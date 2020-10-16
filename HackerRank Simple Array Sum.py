e8135ffee33d5705c4cfdbd3d11857406ad3a149
Vaibhthevampire <68866987+Vaibhthevampire@users.noreply.github.com>
2020-10-16 21:12:21 +0530

    HackerRank Simple Array Sum.py

---
+++ HackerRank Simple Array Sum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    total=0
+    for i in ar:
+        total+=int(i)
+    return(total)    
+if _name_ == '_main_':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
